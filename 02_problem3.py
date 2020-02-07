bac4f3e2cf83a7894c40c11ee21c3091a5b607d9
alessandraseiter <60510059+alessandraseiter@users.noreply.github.com>
2020-02-07 13:49:50 -0500

    add lab1 problem3

---
+++ 02_problem3.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Display the relationship between variables a and b
+a = 1
+b = 2
+if a > b:
+    print('a is greater than b')
+elif a < b:
+    print('a is smaller than b')
+else:
+    print('a is equal to b')
+
+
+# In[ ]:
+
+
+
+
