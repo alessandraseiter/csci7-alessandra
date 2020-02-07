a54d4fed00747a6fbaef1d9ca47164fb55e1a1dc
alessandraseiter <60510059+alessandraseiter@users.noreply.github.com>
2020-02-07 13:37:35 -0500

    add lab1 problem 2

---
+++ 02_problem2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Display which variable is of greater value
+a = 1
+b = 2
+if a > b:
+    print('a is greater than b')
+else:
+    print('a is not greater than b')
+
+
+# In[ ]:
+
+
+
+
