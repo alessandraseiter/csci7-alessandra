6cd285cbacb7f0160e954d824056814b921d05c5
alessandraseiter <60510059+alessandraseiter@users.noreply.github.com>
2020-02-07 17:08:20 -0500

    add lab1 problem5

---
+++ 02_problem5.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#Enter a number and display whether it's a leap year
+
+#Prompt user to enter a year. Verify that it is a valid year, then store it in a variable called user_year
+user_entry = input('Please enter a year.')
+assert( user_entry.isdigit() )
+user_year = int(user_entry)
+
+#Display whether user_year is a leap year
+if user_year % 400 == 0:
+    print(user_year,'is a leap year')
+elif user_year % 4 == 0 and user_year % 100 != 0:
+    print(user_year,'is a leap year')
+else:
+    print(user_year,'is not a leap year')
+
+
+# In[4]:
+
+
+#Enter a number and display whether it's a leap year
+
+#Prompt user to enter a year. Verify that it is a valid year, then store it in a variable called user_year
+user_entry = input('Please enter a year.')
+assert( user_entry.isdigit() )
+user_year = int(user_entry)
+
+#Display whether user_year is a leap year
+if user_year % 400 == 0:
+    print(user_year,'is a leap year')
+elif user_year % 4 == 0 and user_year % 100 != 0:
+    print(user_year,'is a leap year')
+else:
+    print(user_year,'is not a leap year')
+
+
+# In[5]:
+
+
+#Enter a number and display whether it's a leap year
+
+#Prompt user to enter a year. Verify that it is a valid year, then store it in a variable called user_year
+user_entry = input('Please enter a year.')
+assert( user_entry.isdigit() )
+user_year = int(user_entry)
+
+#Display whether user_year is a leap year
+if user_year % 400 == 0:
+    print(user_year,'is a leap year')
+elif user_year % 4 == 0 and user_year % 100 != 0:
+    print(user_year,'is a leap year')
+else:
+    print(user_year,'is not a leap year')
+
+
+# In[6]:
+
+
+#Enter a number and display whether it's a leap year
+
+#Prompt user to enter a year. Verify that it is a valid year, then store it in a variable called user_year
+user_entry = input('Please enter a year.')
+assert( user_entry.isdigit() )
+user_year = int(user_entry)
+
+#Display whether user_year is a leap year
+if user_year % 400 == 0:
+    print(user_year,'is a leap year')
+elif user_year % 4 == 0 and user_year % 100 != 0:
+    print(user_year,'is a leap year')
+else:
+    print(user_year,'is not a leap year')
+
+
+# In[ ]:
+
+
+
+
