c9d6accf36ff7b95d033263ae217b4af882c6340
alessandraseiter <60510059+alessandraseiter@users.noreply.github.com>
2020-02-18 12:04:59 -0500

    add assignment 3 problem 3

---
+++ 03_problem3.py
@@ -0,0 +1,11 @@
+my_list = ['alessandra', 'elaine', 'seiter']
+new_list = my_list
+new_list = my_list[0:3]
+new_list = my_list[0:]
+new_list = my_list[:3]
+new_list = my_list[:]
+new_list = my_list[0]
+new_list = my_list[0:2]
+new_list = my_list[1:]
+new_list = my_list[-1]
+new_list = my_list[:-1]
